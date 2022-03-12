59719a3a6447128737a4a66dc0470a509453f507
Renee Louise Brinkman <reneebrinkman91@gmail.com>
2022-03-12 11:51:37 -0600

    Implement new and detail views

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
 from flask import (render_template,
     url_for, redirect, request)
 from models import db, app, Project
+import datetime
 
+def clean_date(date):
+    date_split = date.split('-')
+    year = int(date_split[0])
+    month = int(date_split[1])
+    completion_date = datetime.date(year=year, month=month, day=1)
+
+    return completion_date
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    projects = Project.query.all()
+    return render_template('index.html', projects=projects)
+
+@app.route('/projects/new', methods=['GET', 'POST'])
+def new_project():
+    if request.form:
+        new_project = Project(
+            title=request.form['title'],
+            date=clean_date(request.form['date']),
+            description=request.form['desc'],
+            skills=request.form['skills'],
+            repo_link=request.form['github'])
+
+        db.session.add(new_project)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    projects=Project.query.all()
+
+    return render_template('projectform.html', projects=projects)
+
+@app.route('/projects/<id>')
+def project_detail(id):
+    projects = Project.query.all()
+    this_project = Project.query.get_or_404(id)
+
+    return render_template('detail.html',
+        projects=projects,
+        this_project=this_project)
 
 if __name__ == '__main__':
     db.create_all()
